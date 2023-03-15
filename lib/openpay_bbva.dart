bdc145327872351863377c3139d59d5fe9fd037d
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-03-15 16:21:13 -0600

    export an Address Model

--- lib/openpay_bbva.dart
+++ lib/openpay_bbva.dart
@@ -1,6 +1,7 @@
 export 'package:openpay_bbva/src/enums/country.dart';
 export 'package:openpay_bbva/src/error/openpay_exception.dart';
 export 'package:openpay_bbva/src/http/openpay_api.dart';
+export 'package:openpay_bbva/src/models/address.dart';
 export 'package:openpay_bbva/src/models/card_information.dart';
 export 'package:openpay_bbva/src/models/token_openpay.dart';
 export 'package:openpay_bbva/src/platforms/openpay_bbva.dart';
